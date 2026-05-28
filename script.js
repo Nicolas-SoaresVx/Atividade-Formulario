50c8cf1b2cca6ae82219514b9914ae9bda1022ec
Nicolas-SoaresVx <soaresnicolas874@gmail.com>
2026-05-28 12:14:02 -0300

    Adicionando mais coisas no JS

--- script.js
+++ script.js
@@ -2,23 +2,41 @@ const quizfut = [
 
 {
     question: "Qual seleção venceu a primeira Copa do Mundo da FIFA?",
-    Option: [Brasil,Uruguai,Italia,Alemanha,japão,mexico],
+    Option: ["Brasil","Uruguai","França","Alemanha","Japão"],
     correct: 1 // Uruguai e o primeiro 
 },
 {
-    question: "",
-    Option: [],
-    correct: 2
+    question: "Quantos jogadores de cada equipe podem estar em campo simultaneamente durante uma partida oficial de futebol?",
+    Option: ["10 jogadores (9 linha 1 gol)",
+             "12 jogadores (11 linha 1 gol)",
+             "5 jogadores (4 linha 1 gol)",
+             "11 jogadores (10 linha 1 gol)",
+             "15 jogadores (14 linha 1 gol)"],
+    correct: 3
 },
 {
-    question: "",
-    Option: [],
-    correct: 2
+    question: "Qual é a duração oficial de uma partida de futebol sénior, excluindo o tempo de compensação e eventuais prolongamentos?",
+    Option:["60 minutos",
+            "100 minutos",
+            "85 minutos",
+            "95 minutos",
+            "90 minutos"],
+    correct: 5
+},
+{
+    question:"Qual destas infrações resulta na marcação de um penálti (grande penalidade)?",
+    Option:["Demorar mais de 6 segundos a repor uma bola lateral",
+            "O ponta de lança simular uma falta na área adversária",
+            "O goleiro agarrar a bola com as mãos fora da sua grande área",
+            "Um jogador cometer uma falta passível de livre direto dentro da sua própria grande área",
+            "Fazer uma falta violenta no meio-campo"
+    ],
+    correct:3
 },
 {
     question:"",
     Option:[],
-    correct:1
+    correct:0
 },
 
 ]
