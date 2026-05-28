24f0ac32aebf15441dca8731fdb13d37a6e9a481
Gustavoandrade19 <gustavooroandraderodrigues@gmail.com>
2026-05-28 11:34:55 -0300

    Adicionando  o JS

---
+++ script.js
@@ -0,0 +1,24 @@
+const quizfut = [
+
+{
+    question: "Qual seleção venceu a primeira Copa do Mundo da FIFA?",
+    Option: [Brasil,Uruguai,Italia,Alemanha,japão,mexico],
+    correct: 1 // Uruguai e o primeiro 
+},
+{
+    question: "",
+    Option: [],
+    correct: 2
+},
+{
+    question: "",
+    Option: [],
+    correct: 2
+},
+{
+    question:"",
+    Option:[],
+    correct:1
+},
+
+]
